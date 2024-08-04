e64e4d4fea66fd24de5a34e920999aa9c360d5ca
Georgii Skorokhod <skorokhod.g@gmail.com>
2024-08-05 02:23:22 +0300

    use persistent storage

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -6,13 +6,14 @@ import type { Task } from "$lib/types/task.ts";
 import type { Shift } from "$lib/types/shift.ts";
 import { appliesTo, type Constraint } from "$lib/types/constraints.ts";
 import { persisted } from "svelte-persisted-store";
+import * as devalue from "devalue";
 
-export const skills = persisted<Skill[]>("skills", []);
-export const employees = persisted<Person[]>("employees", []);
-export const locations = persisted<Location[]>("locations", []);
-export const tasks = persisted<Task[]>("tasks", []);
-export const constraints = persisted<Constraint[]>("constraints", []);
-export const shifts = persisted<Shift[]>("shifts", []);
+export const skills = persisted<Skill[]>("skills", [], { serializer: devalue });
+export const employees = persisted<Person[]>("employees", [], { serializer: devalue });
+export const locations = persisted<Location[]>("locations", [], { serializer: devalue });
+export const tasks = persisted<Task[]>("tasks", [], { serializer: devalue });
+export const constraints = persisted<Constraint[]>("constraints", [], { serializer: devalue });
+export const shifts = persisted<Shift[]>("shifts", [], { serializer: devalue });
 
 export function getConstraintsFor(item: Person | Task | Location): Constraint[] {
   return get(constraints).filter((c) => appliesTo(c, item));
